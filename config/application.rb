e80eeb8a2a1659976c4c6ab2867f757680eb49af
guillaume lhostis <guillaume.lhostis@free.fr>
2023-09-01 11:21:00 +0200

    navbar + basic css rules and components

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Simplertour
       generate.test_framework :test_unit, fixture: false
     end
 
+    config.assets.enabled = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
