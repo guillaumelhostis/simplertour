e1c634f787ee3b0c75a21bc9ebb9d4c9b449ceb7
guillaume lhostis <guillaume.lhostis@free.fr>
2023-10-26 13:46:03 +0200

    fixed tempalte bugs

--- app/controllers/timetable_entries_controller.rb
+++ app/controllers/timetable_entries_controller.rb
@@ -5,15 +5,13 @@ class TimetableEntriesController < ApplicationController
     @tour = Tour.find(params[:tour_id])
     @concert = @tour.concerts.find(params[:concert_id])
     @timeentry = TimetableEntry.new(timetable_entries_params)
-
-
     @timeentry.concert_id = @concert.id
     authorize @timeentry
 
     if @timeentry.save
       redirect_to tour_concert_path(@concert, @tour)
     else
-      redirect_to tour_concert_path(@concert, @tour), notice: 'You have to insert en event'
+      redirect_to tour_concert_path(@concert, @tour), notice: 'You have to insert at least an event and a Start Time'
     end
   end
 
