15783afe3da55e7da06e117ad15833b34a6227f3
guillaume lhostis <guillaume.lhostis@free.fr>
2023-12-13 14:44:11 +0100

    refacto venue controller

---
+++ app/services/venue_creation_service.rb
@@ -0,0 +1,44 @@
+class VenueCreationService
+  include Pundit
+
+  def initialize(venue_params, current_tourman, concert)
+    @venue_params = venue_params
+    @current_tourman = current_tourman
+    @concert = concert
+    @success = false
+  end
+
+  def call
+    build_venue
+    save_venue
+    handle_concert_association
+  end
+
+  def success
+    @success
+  end
+
+  def venue
+    @venue
+  end
+
+  private
+
+  def build_venue
+    @venue = Venue.new(@venue_params.merge(tourman_id: @current_tourman.id))
+  end
+
+  def save_venue_with_files
+    @success = @venue.save
+  end
+
+  def handle_concert_association
+    associate_concert_with_venue if @concert.present?
+  end
+
+  def associate_concert_with_venue
+    @concert.venue_id = @venue.id
+    @concert.name = @venue.name
+    @concert.save
+  end
+end
