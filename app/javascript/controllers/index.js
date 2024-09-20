f755bd5f10860a3f1cf979782d96e1761eacc597
guillaume lhostis <guillaume.lhostis@free.fr>
2024-09-20 14:44:19 +0200

    switch from sprocket to propshaft

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,4 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 console.log("JS is workings");
+console.log("PROPSHAFTWORK");
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Simplertour
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # config.assets.enabled = true
+    # config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
 
     # Configuration for the application, engines, and railties goes here.
     #
