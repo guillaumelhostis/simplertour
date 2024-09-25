415d83ee767d9fda40742796e657d3e7e7400c8a
guillaume lhostis <guillaume.lhostis@free.fr>
2024-09-25 08:50:29 +0200

    change some form label placeholder

--- app/mailers/roadbook_mailer.rb
+++ app/mailers/roadbook_mailer.rb
@@ -1,14 +1,9 @@
 class RoadbookMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.roadbook_mailer.roadbook_email.subject
-  #
+  default from: 'guillaume.lhostis@free.fr'
   def roadbook_email(user)
-
     @user = user
-
+    pdf_data = session[:pdf_data]
+    attachments['Roadbook.pdf'] = File.read(pdf_data.path)
     mail(to: @user.email, subject: 'New Roadbook has been created')
   end
 end
