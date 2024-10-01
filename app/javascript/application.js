5999408019103e358d48d3f9677b5e51335cb1b2
guillaume lhostis <guillaume.lhostis@free.fr>
2024-10-01 12:34:58 +0200

    fixed boostrap alert and front end of login

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "jquery"; // if you're using jQuery
+import "@popperjs/core"; // Import Popper.js if using directly
+import "bootstrap"; // Ensure Bootstrap is imported
 import "controllers"
+// import "bootstrap";
+// import "@popperjs/core"; // Import Popper.js if you're using it directly
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "bootstrap" # @5.3.3
+# pin "bootstrap" # @5.3.3
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
